6af554b04e377cc50592ea6dc2d7e48aa3ba5e74
dylanbartle <dylanbartle@hotmail.com>
2024-04-18 18:25:50 -0600

    new

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const secret = process.env.SECRET;
 const app = express();
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://client-yza1.onrender.com'); // Replace with your frontend URL
+  res.header('Access-Control-Allow-Origin', process.env.CORS_ORIGIN); // Replace with your frontend URL
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Credentials', 'true');
